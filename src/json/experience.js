31f76bb1e76973f9f4cc3c72553f13a3a24a587b
Eci Emriyeni <eciemriyeni@student.ub.ac.id>
2023-12-20 17:53:23 +0700

    Update experience.js

--- src/json/experience.js
+++ src/json/experience.js
@@ -1,6 +1,10 @@
 import filkom from "../img/experience/logo_filkom 1.png"
 import ub from "../img/experience/logo ub 1.png"
+import discite from "../img/experience/Discite Institute.png"
+import dpm from "../img/experience/logo dpm 2.png"
+import kb from "../img/experience/Logo KB.png"
 import Result from "../img/experience/result/example.png"
+
 const experience =[
     {
         img:filkom,
@@ -22,16 +26,34 @@ const experience =[
         date:"Sep 2022 - Okt 2023"
     },
     {
-        img:ub,
+        img:discite,
         title: "Deputy Coordinator of the Data and Developer",
         company :"Discite Institute",
         date:"Aug 2022 - Aug 2023"
     },
     {
-        img:ub,
-        title: "Secretarial Staff - MTQMN XVII 2023",
-        company :"Universitas Brawijaya",
-        date:"Sep 2023 - Nov 2023"
+        img:dpm,
+        title: "Deputy Chief Executive - PEMILWA FILKOM UB",
+        company :"DPM FILKOM UB",
+        date:"Nov 2022 - Jan 2023"
+    },
+    {
+        img:dpm,
+        title: "Advocacy Expert Staff",
+        company :"DPM FILKOM UB",
+        date:"Feb 2022 - Jan 2023"
+    },
+    {
+        img:kb,
+        title: "Staff Publication and IT",
+        company :"EM UB",
+        date:"Aug 2022 - Nov 2022"
+    },
+    {
+        img:dpm,
+        title: "Young Staff Commission 1",
+        company :"DPM FILKOM UB",
+        date:"Nov 2021 - Jan 2022"
     },
 ]
 export default experience
