3bb36d05075985d5fe5a9363bef499e8519530a1
Eci Emriyeni <112626798+eciemriyeni@users.noreply.github.com>
2024-01-03 21:46:07 +0700

    Update uiux.js

--- src/json/uiux.js
+++ src/json/uiux.js
@@ -11,19 +11,44 @@ const uiux = [
     ],
   },
   {
-    title: "Desa Cindogo - Website Application Design",
-    date: "Nov 2023 - Dec 2023",
+    title: "EduRoom - Mobile Application Design",
+    date: "Jan 2023 - Feb 2023",
     skills: [
-      "System Analysis ",
-      "Business Analysis",
       "Figma (Software)",
       "User Experience (UX)",
       "User Interface Design",
     ],
   },
   {
-    title: "Desa Cindogo - Website Application Design",
-    date: "Nov 2023 - Dec 2023",
+    title: "Explore - Mobile Application Design",
+    date: "Nov 2022 - Dec 2022",
+    skills: [
+      "Figma (Software)",
+      "User Experience (UX)",
+      "User Interface Design",
+    ],
+  },
+  {
+    title: "Batara - Mobile Application Design",
+    date: "Oct 2022 - Nov 2022",
+    skills: [
+      "Figma (Software)",
+      "User Experience (UX)",
+      "User Interface Design",
+    ],
+  },
+  {
+    title: "Kampung Budaya - Website Application Design",
+    date: "Aug 2022 - Nov 2022",
+    skills: [
+      "Figma (Software)",
+      "User Experience (UX)",
+      "User Interface Design",
+    ],
+  },
+  {
+    title: "E-Fresh - Website Application Design",
+    date: "Oct 2022 - Nov 2022",
     skills: [
       "System Analysis ",
       "Business Analysis",
@@ -31,6 +56,6 @@ const uiux = [
       "User Experience (UX)",
       "User Interface Design",
     ],
-  },
+  }
 ];
 export default uiux;
