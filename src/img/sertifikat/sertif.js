3fb09fb1faab898d81c7480a2870739420ea746a
Eci Emriyeni <112626798+eciemriyeni@users.noreply.github.com>
2024-01-03 21:55:26 +0700

    Update sertif.js

--- src/img/sertifikat/sertif.js
+++ src/img/sertifikat/sertif.js
@@ -1,4 +1,8 @@
+import discite from "./Certificate of Discite Institute_page-0001.jpg"
+import rakamin2 from "./Certificate of Excellent Grade Rakamin_page-0001.jpg"
+import galoo from "./Certificate of Galoo Store_page-0001.jpg"
 import kb from "./Certificate of Kampung Budaya_page-0001.jpg"
+import bi from "./Certificate of Microsoft Power BI_page-0001.jpg"
 import mtqm from "./Certificate of MTQMN XVII 2023_page-0001.jpg"
 import py1 from "./Certificate of Python (Basic)_page-0001.jpg"
 import py2 from "./Certificate of Python for Data Analysis_page-0001.jpg"
@@ -9,4 +13,5 @@ import sql2 from "./Certificate of SQL Advanced_page-0001.jpg"
 import dpm1 from "./Certificate of Staff Ahli DPM FILKOM UB_page-0001.jpg"
 import dpm2 from "./Certificate of Staff Muda DPM FILKOM UB_page-0001.jpg"
 
-export default {kb, mtqm, py1, py2, rakamin, revou, sql1, sql2, dpm1, dpm2}
+
+export default {discite, rakamin2, galoo, kb, bi, mtqm, py1, py2, rakamin, revou, sql1, sql2, dpm1, dpm2}
